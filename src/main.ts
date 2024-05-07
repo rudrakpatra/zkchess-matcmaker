e629147bba6d4909bbaaec66d9ed3e43392a53a1
rudrakpatra <rudrakpatracodes@gmail.com>
2024-05-07 01:25:23 +0000

    env update

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,36 @@ import { Server } from 'socket.io'
 import { createServer } from 'http'
 import cors from 'cors'
 import { socketConnectionHandler as handleSocketConnection } from './webSocket'
+import * as dotenv from "dotenv"
+dotenv.config();
 
 //express
 const app: Application = express()
 
 const PORT = 8080
-const SERVER_URL = 'http://localhost'
+const vercelSettings="https://vercel.com/rudrakpatras-projects/zkchess/settings/environment-variables"
+const workspaceUrl=process.env.GITPOD_WORKSPACE_URL
+const html=`
+    <h1>
+      REMINDER: 
+    </h1>
+    <a href="${vercelSettings}">
+      Set the environment variables in vercel
+    </a>
+		<br/>
+		<h5>SET PUBLIC_MATCHMAKER_URL to this:</h5>
+		<h2>
+      ${workspaceUrl}
+    <h2>
+`
 app.use(cors())
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('health check')
+  res.status(200).send(html)
 })
 
 const server = createServer(app)
 server.listen(PORT, () => {
-  console.log(`Service starting at ${SERVER_URL}:${PORT}`)
+  console.log(`Service starting at ${process.env.GITPOD_WORKSPACE_URL}`)
 })
 // app.listen(PORT, () => {
 //   console.info(`Service starting at ${SERVER_URL}:${PORT}`)
